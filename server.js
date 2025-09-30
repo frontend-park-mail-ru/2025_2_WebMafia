eacf82693a408ce103fd5a104f485567d25f98d9
xyzdat <agolubev04@mail.ru>
2025-09-30 17:15:48 +0300

    добавил наблоски плеера, немнгого поменял дизайн самой страницы

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ app.use(/(.*)/, (req, res) => {
     res.sendFile(`${__dirname}/public/index.html`);
 });
 
-
 app.listen(process.env.PORT || 8090, () => {
-    console.log(`Express server is listening on :${port}`);
+    console.log(`Express server is listening on http://localhost:${port}`);
 });
