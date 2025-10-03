1bc711d784aba4fd04f6588d1dadb51cece3c38e
xyzdat <agolubev04@mail.ru>
2025-10-03 13:23:43 +0300

    linter

--- public/app.js
+++ public/app.js
@@ -1,9 +1,9 @@
 import { router } from './static/js/routing.js';
 
-document.addEventListener('DOMContentLoaded', function() {
-    initializePage();
+document.addEventListener('DOMContentLoaded', function () {
+  initializePage();
 });
 
-function initializePage(){
-    router.init();
+function initializePage() {
+  router.init();
 }
