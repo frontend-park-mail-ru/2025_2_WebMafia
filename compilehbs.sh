19e0180987bd6416250eed77454360aed2ff8331
Дорофеев Дмитрий WEB-21 <dimma.dort@gmail.com>
2025-10-10 14:40:27 +0300

    Profile page base js and layout

--- compilehbs.sh
+++ compilehbs.sh
@@ -6,6 +6,10 @@ handlebars public/static/js/pages/login/login.hbs -f public/static/js/pages/logi
 
 handlebars public/static/js/pages/register/register.hbs -f public/static/js/pages/register/register.tmpl.js
 
+handlebars public/static/js/pages/profile/profilePage.hbs -f public/static/js/pages/profile/profilePage.tmpl.js
+
 handlebars public/static/js/partials/eyeOpen.hbs -f public/static/js/partials/eyeOpen.tmpl.js
 
 handlebars public/static/js/partials/eyeClosed.hbs -f public/static/js/partials/eyeClosed.tmpl.js
+
+handlebars public/static/js/partials/pencil.hbs -f public/static/js/partials/pencil.tmpl.js
--- public/app.js
+++ public/app.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
 function registerPartials() {
   Handlebars.registerPartial('eyeOpen', Handlebars.templates['eyeOpen.hbs']);
   Handlebars.registerPartial('eyeClosed', Handlebars.templates['eyeClosed.hbs']);
+  Handlebars.registerPartial('pencil', Handlebars.templates['pencil.hbs']);
 }
 
 function initializePage() {
