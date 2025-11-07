7c5d49952aacb1725991ad2d3f84ad99f4973ec9
xyzdat <agolubev04@mail.ru>
2025-11-07 17:40:36 +0300

    fix player

--- public/static/js/pages/player/player.js
+++ public/static/js/pages/player/player.js
@@ -164,7 +164,7 @@ export class Player extends EventTarget {
     const durationFormatted = `${minutes}:${seconds.toString().padStart(2, '0')}`;
     document.querySelector('.track-time.total').textContent = durationFormatted;
 
-    document.querySelector('.track-cover-player').src = getValidImage(track?.album?.avatar_url, 'default-album.png');
+    document.querySelector('.track-cover-player').src = getValidImage('albums/' + track?.album?.avatar_url, 'default-album.png');
 
     let file_url = track.file_url;
     this.audio.src = file_url ? `${API_TRACKS_URL}/${file_url}` : `static/music/${file_url}`;
