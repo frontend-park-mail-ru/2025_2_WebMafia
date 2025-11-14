971570a9d3bbbb01e3ffaf69a8cbad29ecfbe0b3
poopaapopa <dimma.dort@gmail.com>
2025-11-14 19:36:36 +0300

    data API

--- public/static/js/data.js
+++ public/static/js/data.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = 'http://localhost:8080/api/v1';
-export const API_AVATARS_URL = 'http://localhost:8099/avatars';
+const API_BASE_URL = 'http://217.16.17.173:8080/api/v1';
+export const API_AVATARS_URL = 'http://217.16.17.173:8099/avatars';
 export const API_TRACKS_URL = 'http://217.16.17.173:8099/music/tracks';
 
 export class apiServises {
