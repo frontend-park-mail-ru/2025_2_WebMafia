81a229351637e141870af756c1e0f1eeaa0e2139
xyzdat <agolubev04@mail.ru>
2025-11-15 18:09:38 +0300

    fix

--- public/static/js/data.js
+++ public/static/js/data.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8080/api/v1';
+const API_BASE_URL = 'http://217.16.17.173:8080/api/v1';
 export const API_AVATARS_URL = 'http://217.16.17.173:8099/avatars';
 export const API_TRACKS_URL = 'http://217.16.17.173:8099/music/tracks';
 
