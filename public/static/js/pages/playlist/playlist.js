bdf76fc54c3a5e8d8e40128dc1ab22d436bc3664
xyzdat <agolubev04@mail.ru>
2025-11-26 19:14:03 +0300

    play fix

--- public/static/js/pages/playlist/playlist.js
+++ public/static/js/pages/playlist/playlist.js
@@ -402,20 +402,21 @@ export class PlaylistPage {
 
     const dotsBtn = document.getElementById('playlistActions');
     const menu = document.getElementById('playlistMenu');
+    if (dotsBtn) {
+      dotsBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        menu.classList.toggle('hidden');
 
-    dotsBtn.addEventListener('click', (e) => {
-      e.preventDefault();
-      menu.classList.toggle('hidden');
+        const rect = dotsBtn.getBoundingClientRect();
+        const parentRect = dotsBtn.parentElement.getBoundingClientRect();
 
-      const rect = dotsBtn.getBoundingClientRect();
-      const parentRect = dotsBtn.parentElement.getBoundingClientRect();
+        const top = rect.top - parentRect.top - menu.offsetHeight - 6;
+        const left = rect.left - parentRect.left - 10;
 
-      const top = rect.top - parentRect.top - menu.offsetHeight - 6;
-      const left = rect.left - parentRect.left - 10;
-
-      menu.style.top = `${top}px`;
-      menu.style.left = `${left}px`;
-    });
+        menu.style.top = `${top}px`;
+        menu.style.left = `${left}px`;
+      });
+    }
 
     document.addEventListener('click', (e) => {
       if (!menu.contains(e.target) && !dotsBtn.contains(e.target)) {
