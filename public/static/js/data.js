6a36cffabab53bf10695192b128e278565181024
xyzdat <agolubev04@mail.ru>
2025-12-03 17:21:54 +0300

    serv path

--- public/static/js/data.js
+++ public/static/js/data.js
@@ -1,8 +1,8 @@
-const path = 'localhost';
+const path = 'wave-music.ru';
 
-const API_BASE_URL = `http://${path}:8080/api/v1`;
-const API_Data_URL = `http://${path}:8081/api/v1`;
-const API_PLAYLIST_URL = `http://${path}:8082/api/v1`;
+const API_BASE_URL = `http://${path}/api/v1`;
+const API_Data_URL = `http://${path}/api/v1`;
+const API_PLAYLIST_URL = `http://${path}/api/v1`;
 export const API_AVATARS_URL = 'http://217.16.17.173:8099/avatars';
 export const API_TRACKS_URL = 'http://217.16.17.173:8099/music/tracks';
 
