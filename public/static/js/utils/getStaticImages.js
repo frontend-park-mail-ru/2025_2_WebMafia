c01ed6d5d553d5d0e7d5fa8a3bf2ab99a09588af
xyzdat <agolubev04@mail.ru>
2025-12-06 14:19:01 +0300

    fix static images

--- public/static/js/utils/getStaticImages.js
+++ public/static/js/utils/getStaticImages.js
@@ -1,16 +1,19 @@
 export function getStaticImagePath(apiPath) {
   const logoImage = document.querySelector('.wave-icon');
   const defaultPlaylist = document.querySelector('.profile-image');
+  console.log(apiPath);
   if (logoImage) {
-    if (apiPath === 'wave-music.ru') {
+    if (apiPath == 'wave-music.ru') {
       logoImage.src = 'assets/logo-F_j9P4rl.png';
+    } else {
+      logoImage.src = 'static/img/logo.png';
     }
-    logoImage.src = 'static/img/logo.png';
   }
   if (defaultPlaylist) {
     if (apiPath === 'wave-music.ru') {
-      defaultPlaylist.src = 'default-playlist-F_j9P4rl.png';
+      defaultPlaylist.src = 'assets/default-playlist-F_j9P4rl.png';
+    } else {
+      defaultPlaylist.src = 'static/img/default-playlist.png';
     }
-    defaultPlaylist.src = 'static/img/default-playlist.png';
   }
 }
