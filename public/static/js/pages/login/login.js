a2aa05b379949dc6216efcc17fee2f70147ec6b6
xyzdat <agolubev04@mail.ru>
2025-12-17 01:03:31 +0300

    пофиксил проблемы с созданием, редактом плейлистов добавлеинем прослушивания

--- public/static/js/pages/login/login.js
+++ public/static/js/pages/login/login.js
@@ -58,6 +58,7 @@ export class LoginPage {
 
         localStorage.setItem('isAuthenticated', 'true');
         localStorage.setItem('uid', response.id);
+        apiServise.csrfToken = null;
 
         router.navigate('/');
       } catch (error) {
